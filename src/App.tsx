47bccae101417d7b35c421cdc80d5953c12ee355
VyD <vy.dinh.2929@gmail.com>
2025-07-01 23:43:23 +0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "@styles/index.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-5xl text-red">HELLO</h1>
+        <h1 className="text-5xl text-red">HELLO</h1>
+        <h1 className="text-5xl text-red">HELLO</h1>
+        <h1 className="text-5xl text-red">HELLO</h1>
+        <h1 className="text-5xl text-red">HELLO</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
