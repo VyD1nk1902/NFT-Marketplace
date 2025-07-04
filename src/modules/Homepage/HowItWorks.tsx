6b541a102bfc9927e2950073abed1e1eae3cf164
VyDink <vy.dinh.2929@gmail.com>
2025-07-04 16:03:00 +0700

    HowItWorks module section

---
+++ src/modules/Homepage/HowItWorks.tsx
@@ -0,0 +1,20 @@
+const HowItWorks = () => {
+  const HIWCarData = [
+    {
+      img: "#",
+      title: "#",
+      desc: "#",
+    },
+  ];
+  return (
+    <section>
+      <div>
+        <h3>How it works</h3>
+        <p>Find out how to get started</p>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default HowItWorks;
