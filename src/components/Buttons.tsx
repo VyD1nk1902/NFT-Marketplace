0d5759096b17c3a76d7aac23702d704defcb3852
VyDink <vy.dinh.2929@gmail.com>
2025-07-04 15:26:14 +0700

    Component Button

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,12 +1,5 @@
 import type { ButtonProps } from "@myTypes/Buttons/ButtonTypes";
-const Buttons = ({
-  size = "secondary",
-  background = "color",
-  className,
-  children,
-  onClick,
-  link,
-}: ButtonProps) => {
+const Buttons = ({ size = "secondary", background = "color", className, children, onClick, link }: ButtonProps) => {
   const sizeOptions = {
     primary: " h-[72px] text-[22px] font-[600]",
     secondary: " h-[60px] text-[16px] font-[600]",
@@ -21,24 +14,17 @@ const Buttons = ({
   const buttonSize = sizeOptions[size] || sizeOptions.secondary;
   const buttonColor = colorOptions[background] || colorOptions.color;
 
-  const buttonOptionClass = `flex gap-3 text-text rounded-[20px] cursor-pointer justify-center items-center scale-105 transition-all ease-in-out duration-300 hover:scale-100 ${buttonSize} ${buttonColor} `;
+  const buttonOptionClass = `flex gap-3 rounded-[20px] cursor-pointer justify-center items-center scale-105 transition-all ease-in-out duration-300 hover:scale-100 ${buttonSize} ${buttonColor} `;
 
   if (link === "")
     return (
-      <button
-        onClick={onClick}
-        className={`${buttonOptionClass}, ${className}`}
-      >
+      <button onClick={onClick} className={`${buttonOptionClass}, ${className}`}>
         {children}
       </button>
     );
 
   return (
-    <a
-      href={link}
-      onClick={onClick}
-      className={`${buttonOptionClass}, ${className}`}
-    >
+    <a href={link} onClick={onClick} className={`${buttonOptionClass}, ${className}`}>
       {children}
     </a>
   );
