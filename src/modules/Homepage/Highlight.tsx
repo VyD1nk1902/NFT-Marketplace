86e79487ee940588d1a39b9e2d81e977af929001
VyDink <vy.dinh.2929@gmail.com>
2025-07-09 15:53:30 +0700

    This module contain a background img, a filter overlay and the main content. In the reponsive figma design, on mobile layout, clock appears differently than 2 others layout, so here I use useMemo to make sure time won't be re-render after each layout. And for the position of the clock, I use component to import clock and react-reponsive instead of block-hidden CSS for conditional rendering because it prevents multiple Clocks from being mounted and avoids timer resets or glitches.

--- src/modules/Homepage/Highlight.tsx
+++ src/modules/Homepage/Highlight.tsx
@@ -3,28 +3,53 @@ import Buttons from "@components/Buttons";
 import { Eye } from "@assets/Svg/SvgExport";
 import Avatar from "@assets/Avatars/Avatar Placeholder (6).png";
 
-import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
-import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+import { useMemo } from "react";
+import { useMediaQuery } from "react-responsive";
+
+import Clock from "@components/Clock";
 
 const Highlight = () => {
+  // Mobile: < 640px
+  const isMobile = useMediaQuery({ maxWidth: 639 });
+
+  // Tablet: >= 640px và < 1024px
+  const isTablet = useMediaQuery({ minWidth: 640, maxWidth: 1023 });
+
+  // Desktop: >= 1024px
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+
+  // Make sure time not reset after render
+  const timeEnd = useMemo(() => {
+    return new Date().getTime() + 24 * 3600 * 1000 * 2.5;
+  }, []);
+
+  // The clock positions is different depending on the responsive layout.
+  // React will re-render (unmount/mount) the Clock component when the layout changes,
+  // so we use useMemo to ensure the countdown time stays the same across renders.
+  // Using react-responsive for conditional rendering is better than relying on
+  // CSS (hidden/block/flex) because it prevents multiple Clocks from being mounted
+  // and avoids timer resets or glitches.
+
   return (
     <section>
-      <div className="relative w-full h-[640px] overflow-hidden">
+      <div className="relative w-full  overflow-hidden">
         {/* Background img */}
         <img src={HighlightBg} className="w-full h-full object-cover object-center absolute inset-0" alt="Background" />
         {/* Background filter */}
-        <div className="absolute inset-0 bg-gradient3 blur-3xl "></div>
+        <div className="absolute inset-0 bg-gradient3 sm:blur-3xl blur"></div>
         {/* Content */}
-        <div className="relative z-30 w-[1050px] mx-auto pt-[360px] pb-[60px]">
-          <div className="flex justify-between items-end">
+        <div className="relative z-30 lg:max-w-[1050px] sm:max-w-[690px] max-w-[315px] m-auto md:pt-[360px] pt-[120px] mb-[100px] ">
+          <div className="flex justify-between md:items-end">
             <div className="flex flex-col gap-[30px]">
               <a href="#" className="flex px-5 py-2.5 bg-bg-secondary w-[151px] rounded-[20px] gap-3 hover-scale">
                 <img src={Avatar} className="w-6 h-6" alt="" />
                 <span>Schroomie</span>
               </a>
               <h2>Magic Mashrooms</h2>
+              {/* Clock-mobile */}
+              {isMobile && <Clock time={timeEnd} className="flip-clock-mobile" day={isTablet || isDesktop} />}
               <Buttons
-                className="w-[198px] px-[50px] py-[22px] text-black bg-text"
+                className="sm:w-[198px] w-full px-[50px] py-[22px] text-black bg-text"
                 link="#"
                 size="secondary"
                 background="transparent"
@@ -34,18 +59,8 @@ const Highlight = () => {
               </Buttons>
             </div>
             {/* Clock */}
-            <div className="relative p-[30px] font-space-mono flex flex-col gap-[30px] ">
-              <div className="absolute inset-0 bg-[#3B3B3B80] rounded-[20px]"></div>
-              <div className="relative z-10">
-                <p className="text-xs pb-5">Auction ends in:</p>
-                <FlipClockCountdown
-                  to={new Date().getTime() + 24 * 3600 * 1000 * 2.5}
-                  renderMap={[true, true, true, true]}
-                >
-                  Finished
-                </FlipClockCountdown>
-              </div>
-            </div>
+            {isTablet && <Clock time={timeEnd} className="flip-clock-tablet" day={isTablet || isDesktop} />}
+            {isDesktop && <Clock time={timeEnd} className="" day={isTablet || isDesktop} />}
           </div>
         </div>
       </div>
