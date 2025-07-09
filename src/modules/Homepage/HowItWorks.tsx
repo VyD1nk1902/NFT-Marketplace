15592ee37f33ea27a3bf1d4bfab855615e11220f
VyDink <vy.dinh.2929@gmail.com>
2025-07-09 17:31:35 +0700

    Reponsive for HowItWork module. Im using reponsive hook and dynamic padding by breakpoint

--- src/modules/Homepage/HowItWorks.tsx
+++ src/modules/Homepage/HowItWorks.tsx
@@ -5,6 +5,7 @@ import Collection2x from "@assets/Homepage/HowItWorks/Create Collection Icon@2x.
 import Earning from "@assets/Homepage/HowItWorks/Start Earning Icon.png";
 import Earning2x from "@assets/Homepage/HowItWorks/Start Earning Icon@2x.png";
 import { v4 as uuidv4 } from "uuid";
+import { useReponsive } from "@hooks/useReponsive";
 
 const HowItWorks = () => {
   const HIWCarData = [
@@ -30,23 +31,72 @@ const HowItWorks = () => {
       desc: "Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.",
     },
   ];
+
+  const { isMobile: Mobile, isTablet: Tablet, isDesktop: Desktop } = useReponsive();
+
+  let pxClass = "";
+  if (Mobile) pxClass = "p-5";
+  if (Tablet) pxClass = "pt-2.5 pb-[30px] px-5";
+  if (Desktop) pxClass = "pt-2.5 pb-[30px] px-[30px]";
+
   return (
     <section className="content-wrapper flex flex-col gap-12">
       <div className="flex flex-col gap-2.5">
-        <h3 className="capitalize">How it works</h3>
-        <p className="text-[22px] capitalize ">Find out how to get started</p>
+        {Desktop ? <h3 className="capitalize">How it works</h3> : <h4 className="capitalize">How it works</h4>}
+        {Desktop ? (
+          <p className="text-[22px] capitalize ">Find out how to get started</p>
+        ) : (
+          <p className="text-base capitalize ">Find out how to get started</p>
+        )}
       </div>
-      <div className="grid grid-cols-3 gap-[30px]">
+      <div className="grid sm:grid-cols-3 grid-cols-1 gap-[30px]">
         {HIWCarData.map((item) => (
           <div
             key={item.id}
-            className="flex flex-col gap-5 items-center pt-2.5 pb-[30px] px-[30px] bg-bg-secondary rounded-[20px]"
+            className={`flex sm:flex-col gap-5 ${pxClass}  items-center bg-bg-secondary rounded-[20px]`}
           >
-            <img src={item.img} srcSet={`${item.img} 1x, ${item.img2x} 2x`} alt="" />
-            <div className="text-center flex flex-col gap-2.5">
-              <h5>{item.title}</h5>
-              <p>{item.desc}</p>
-            </div>
+            {Desktop && (
+              <>
+                <img
+                  src={item.img}
+                  srcSet={`${item.img} 1x, ${item.img2x} 2x`}
+                  className="w-[250px] h-[250px]"
+                  alt=""
+                />
+                <div className="text-center flex flex-col gap-2.5">
+                  <h5>{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </>
+            )}
+            {Tablet && (
+              <>
+                <img
+                  src={item.img}
+                  srcSet={`${item.img} 1x, ${item.img2x} 2x`}
+                  className="w-[160px] h-[160px]"
+                  alt=""
+                />
+                <div className="text-center flex flex-col gap-2.5">
+                  <p className="font-[600]">{item.title}</p>
+                  <p className="text-[12px]">{item.desc}</p>
+                </div>
+              </>
+            )}
+            {Mobile && (
+              <>
+                <img
+                  src={item.img}
+                  srcSet={`${item.img} 1x, ${item.img2x} 2x`}
+                  className="w-[100px] h-[100px]"
+                  alt=""
+                />
+                <div className=" flex flex-col gap-2.5">
+                  <p className="font-[600]">{item.title}</p>
+                  <p className="text-[12px]">{item.desc}</p>
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
