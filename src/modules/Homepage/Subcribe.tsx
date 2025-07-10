c2c1b700432b76cb81e6b2fdebe2cc5ac9dfb34e
VyD <vy.dinh.2929@gmail.com>
2025-07-10 14:06:35 +0700

    Reponsive and comment

--- src/modules/Homepage/Subcribe.tsx
+++ src/modules/Homepage/Subcribe.tsx
@@ -1,24 +1,31 @@
 import Inputs from "@components/Inputs";
 import Buttons from "@components/Buttons";
+
 import Photo from "@assets/Homepage/Subcribe/Photo.png";
 import Photo2x from "@assets/Homepage/Subcribe/Photo@2x.png";
+
 import { Email } from "@assets/Svg/SvgExport";
 import { useReponsive } from "@hooks/useReponsive";
 
 const Subcribe = () => {
+  // Reponsive wrap div variables
   const { isMobile: Mobile, isTablet: Tablet, isDesktop: Desktop } = useReponsive();
   let borderClass = "";
   if (Desktop) borderClass = "bg-bg-secondary p-[60px] rounded-[20px] flex gap-[80px]";
   if (Tablet) borderClass = "bg-bg-secondary px-[30px] py-[40px] rounded-[20px] flex gap-[30px]";
-  if (Mobile) borderClass = "p-[60px] rounded-[20px] flex gap-[80px]";
+  if (Mobile) borderClass = " flex flex-col gap-[30px]";
 
   return (
     <section className="content-wrapper">
       <div className={`${borderClass}`}>
+        {/* Left Pic */}
         {Desktop && <img src={Photo} srcSet={`${Photo} 1x , ${Photo2x}`} className="w-[425px] h-[310px]" alt="" />}
         {Tablet && <img src={Photo} srcSet={`${Photo} 1x , ${Photo2x}`} className="w-[300px] h-[280px]" alt="" />}
         {Mobile && <img src={Photo} srcSet={`${Photo} 1x , ${Photo2x}`} className="w-[315px] h-[255px]" alt="" />}
+
+        {/* Content */}
         <div>
+          {/* Desc */}
           {Desktop ? (
             <div className="capitalize">
               <h3>
@@ -41,6 +48,7 @@ const Subcribe = () => {
             </div>
           )}
 
+          {/* Input button */}
           {Desktop && (
             <div className="bg-white rounded-[20px] flex gap-5">
               <Inputs
@@ -59,14 +67,31 @@ const Subcribe = () => {
           )}
 
           {Tablet && (
-            <div className=" flex-col rounded-[20px]  gap-4">
+            <div className="flex flex-col rounded-[20px] gap-4">
               <Inputs
                 id="email"
                 name="email"
                 autoComplete="email"
                 placeholder="Enter your email here..."
                 type="text"
-                className=" bg-white px-5 py-4 w-full h-[46px] text-bg-primary rounded-[20px] border-none  "
+                className=" bg-white px-5 py-4 h-[46px] text-bg-primary rounded-[20px] border-none  "
+              />
+
+              <Buttons className="w-[300px]" size="tertiary" background="color" link="#">
+                <Email className="fill-text w-5 h-5" /> Subcribe
+              </Buttons>
+            </div>
+          )}
+
+          {Mobile && (
+            <div className="flex flex-col rounded-[20px] gap-4">
+              <Inputs
+                id="email"
+                name="email"
+                autoComplete="email"
+                placeholder="Enter your email here..."
+                type="text"
+                className=" bg-white px-5 py-4 h-[46px] text-bg-primary rounded-[20px] border-none  "
               />
 
               <Buttons className="w-full" size="tertiary" background="color" link="#">
