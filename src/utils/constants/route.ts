75825d7fa3364472ce1837f5c235cbe9458da818
VyD <vy.dinh.2929@gmail.com>
2025-07-14 21:09:38 +0700

    feat(routing): Centralize routes and implement dynamic path generation
    Introduces a dedicated `ROUTES` constant to centralize all application paths, enhancing maintainability and reducing hardcoded string errors.
    
    Implements a `getPath` utility function for generating dynamic URLs. This function:
    - Safely replaces placeholders (e.g., `:id`) with provided parameters.
    - Uses `Record` for flexible parameter typing, accommodating various dynamic segments.
    - Ensures robustness by checking for own properties using `hasOwnProperty`.
    
    This change significantly improves route management, readability, and future refactoring efforts.

---
+++ src/utils/constants/route.ts
@@ -0,0 +1,40 @@
+export const ROUTES = {
+  HOME: "/",
+  CREATE_ACCOUNT: "/create-account",
+  MARKETPLACE: "/marketplace",
+  RANKINGS: "/rankings",
+  CONNECT_WALLET: "/connect-wallet",
+
+  //Dynamic routes
+  ITEM_DETAILS: "/item/:id",
+  ARTIST_DETAILS: "/artist/:id",
+
+  //placeholder :id
+};
+
+//Dynamic getPath function
+
+interface getPathProps {
+  pathConstant: string;
+  params: Record<string, string | number>;
+}
+// pathConstant: base path
+// params: parameters ( value for :id)
+// use Record here to describe object whose keys and values as string and number without knowing in advance their name (no need to list type of params)
+
+export const getPath = ({ pathConstant, params }: getPathProps) => {
+  if (!params) {
+    return pathConstant;
+  }
+
+  let resultPath = pathConstant;
+  for (const key in params) {
+    if (Object.prototype.hasOwnProperty.call(params, key)) {
+      resultPath = resultPath.replace(`:${key}`, String(params[key]));
+    }
+    // (if...)Make sure code run for key in params not other prototype properties
+    // replace placeholder with value of the key (id: 123)
+    // use String(params[key] make sure value is string because replace() expect string
+  }
+  return resultPath;
+};
