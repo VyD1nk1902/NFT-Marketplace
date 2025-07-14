6f8df601bd75425656cd40eca7d6d1f3ae76b826
VyD <vy.dinh.2929@gmail.com>
2025-07-14 18:00:59 +0700

    Ưpdate global components and add new page, prepare for using router route for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import "@styles/index.css";
+
+//Global Components
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import ModalHeader from "@components/ModalHeader";
+
+//Pages
 import Homepage from "@pages/Homepage";
+import CreateAccount from "@pages/CreateAccount";
+
 function App() {
   return (
-    <>
-      <div>
-        <Homepage />
-      </div>
-    </>
+    <div>
+      <Header />
+      <ModalHeader />
+      <Homepage />
+      <CreateAccount />
+      <Footer />
+    </div>
   );
 }
 
