9d03d6b85f03243add487d105751254f2ccc77c9
VyD <vy.dinh.2929@gmail.com>
2025-07-17 19:09:28 +0700

    Add comment function

--- src/components/FormatBalance.tsx
+++ src/components/FormatBalance.tsx
@@ -2,3 +2,5 @@ export const FormatBalance = (balance: number | string): string => {
   const numericBalance = typeof balance === "string" ? parseFloat(balance) : balance;
   return numericBalance.toFixed(4);
 };
+
+// This function change type of string value of API to number and use toFixed to show 4 number after decimal point
