a99057056e426288c3456335ac6d74f3393774f3
VyD <vy.dinh.2929@gmail.com>
2025-07-20 20:08:09 +0700

    feat: Update using mockoon API mocking
    - Use Zustand useFetchingStore to fetching API from mockoon through useEffect.
    - Add route path for navigate using Link to and `useNavigate()`.
    - Refactor local api now images is local and others data on mockoon.
    - Use FormatBalance component to refactor decimal balance value.
    - Add loading and error popup state

--- src/modules/Homepage/Discovery.tsx
+++ src/modules/Homepage/Discovery.tsx
@@ -10,44 +10,124 @@ import DiscoveryNFT2large from "@assets/Homepage/Discovery/DiscoveryNFT(2)@2x.pn
 import DiscoveryNFT3small from "@assets/Homepage/Discovery/DiscoveryNFT(3).png";
 import DiscoveryNFT3large from "@assets/Homepage/Discovery/DiscoveryNFT(3)@2x.png";
 
-import { v4 as uuidv4 } from "uuid";
-
 import { Link } from "react-router-dom";
 
+import type { DiscoveryMetaData, LocalDiscoveryImage, CombinedDiscoveryMetaData } from "@myTypes/api";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDiscoveryStore } from "@stores/useFetchingStore";
+
+import { ROUTES } from "@utils/constants/route";
+import { useNavigate } from "react-router-dom";
+
+import { FormatBalance } from "@components/FormatBalance";
+
+// Local image NFT data
+const localImageNFT: Record<string, LocalDiscoveryImage> = {
+  discovery1: {
+    background1x: DiscoveryNFT1small,
+    background2x: DiscoveryNFT1large,
+    userImg: NFTavatar,
+  },
+  discovery2: {
+    background1x: DiscoveryNFT2small,
+    background2x: DiscoveryNFT2large,
+    userImg: NFTavatar1,
+  },
+  discovery3: {
+    background1x: DiscoveryNFT3small,
+    background2x: DiscoveryNFT3large,
+    userImg: NFTavatar2,
+  },
+};
+
 const DiscoveryMore = () => {
-  // Cards list data render
-  const discoveryCardContents = [
-    {
-      id: uuidv4(),
-      background1x: DiscoveryNFT1small,
-      background2x: DiscoveryNFT1large,
-      userImg: NFTavatar,
-      userName: "MoonDancer",
-      nftName: "Distant Galaxy",
-      price: 1.63,
-      bid: 0.33,
-    },
-    {
-      id: uuidv4(),
-      background1x: DiscoveryNFT2small,
-      background2x: DiscoveryNFT2large,
-      userImg: NFTavatar1,
-      userName: "NebulaKid",
-      nftName: "Life On Edena",
-      price: 1.63,
-      bid: 0.33,
-    },
-    {
-      id: uuidv4(),
-      background1x: DiscoveryNFT3small,
-      background2x: DiscoveryNFT3large,
-      userImg: NFTavatar2,
-      userName: "Spaceone",
-      nftName: "AstroFiction",
-      price: 1.63,
-      bid: 0.33,
-    },
-  ];
+  // Cards list NFT local data
+  // const discoveryCardContents = [
+  //   {
+  //     id: uuidv4(),
+  //     background1x: DiscoveryNFT1small,
+  //     background2x: DiscoveryNFT1large,
+  //     userImg: NFTavatar,
+  //     userName: "MoonDancer",
+  //     nftName: "Distant Galaxy",
+  //     price: 1.63,
+  //     bid: 0.33,
+  //   },
+  //   {
+  //     id: uuidv4(),
+  //     background1x: DiscoveryNFT2small,
+  //     background2x: DiscoveryNFT2large,
+  //     userImg: NFTavatar1,
+  //     userName: "NebulaKid",
+  //     nftName: "Life On Edena",
+  //     price: 1.63,
+  //     bid: 0.33,
+  //   },
+  //   {
+  //     id: uuidv4(),
+  //     background1x: DiscoveryNFT3small,
+  //     background2x: DiscoveryNFT3large,
+  //     userImg: NFTavatar2,
+  //     userName: "Spaceone",
+  //     nftName: "AstroFiction",
+  //     price: 1.63,
+  //     bid: 0.33,
+  //   },
+  // ];
+
+  const navigateBtn = useNavigate();
+  const handleSeeAll = () => navigateBtn(ROUTES.MARKETPLACE);
+
+  const data = useDiscoveryStore((state) => state.data);
+  const loading = useDiscoveryStore((state) => state.loading);
+  const error = useDiscoveryStore((state) => state.error);
+  const fetchData = useDiscoveryStore((state) => state.fetchData);
+
+  useEffect(() => {
+    const fetchDiscoveryData = async () => {
+      const response = await axios.get<DiscoveryMetaData[]>("http://localhost:3001/discovery-metadata");
+      const fetchedDiscoveryMetaData = response.data;
+
+      const combinedData: CombinedDiscoveryMetaData[] = fetchedDiscoveryMetaData.map((item) => ({
+        ...item,
+        ...localImageNFT[item.imageKey],
+      }));
+      return combinedData;
+    };
+    fetchData(fetchDiscoveryData);
+  }, [fetchData]);
+  // This effect depends on the fetchData function from the Zustand store.
+  // Need include it here [dependency] to follow React's hook rules
+  // I include `fetchData` in this array because I use it inside the effect.
+  // Even though it's here, the effect will only run once because `fetchData` is a stable function from Zustand that never changes.
+
+  // --- Render ReactLoading component  ---
+  if (loading) {
+    return (
+      <section className=" min-h-[300px] flex justify-center items-center flex-col gap-4">
+        <p className="text-xl text-action">Loading NFTs...</p>
+      </section>
+    );
+  }
+
+  // --- Render Error ---
+  if (error) {
+    return (
+      <section className="min-h-[300px] flex justify-center items-center text-red-500">
+        <p className="text-xl">An error occurred while loading NFTs: {error}</p>
+      </section>
+    );
+  }
+
+  // --- Add this check to prevent the error data null ---
+  if (!data || data.length === 0) {
+    return (
+      <section className="min-h-[300px] flex justify-center items-center">
+        <p className="text-xl text-caption-label">No NFTs found.</p>
+      </section>
+    );
+  }
 
   return (
     <section className="content-wrapper flex flex-col gap-[40px] lg:gap-[60px]">
@@ -58,7 +138,12 @@ const DiscoveryMore = () => {
           <h4 className="block lg:hidden">Discover More NFTs</h4>
           <p className="lg:text-[22px] capitalize">Explore new trending NFTs</p>
         </div>
-        <Buttons className="px-[50px] mt-4.5 mr-1 hidden sm:flex" size="secondary" background="transparent" to="/">
+        <Buttons
+          className="px-[50px] mt-4.5 mr-1 hidden sm:flex"
+          size="secondary"
+          background="transparent"
+          onClick={handleSeeAll}
+        >
           <Eye className="fill-action w-[20px] h-[20px]" />
           See All
         </Buttons>
@@ -66,8 +151,8 @@ const DiscoveryMore = () => {
 
       {/* Cards list */}
       <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[30px] ">
-        {discoveryCardContents.map((item) => (
-          <Link to="/" key={item.id} className="hover-scale ">
+        {data.map((item) => (
+          <Link to={ROUTES.NFT_DETAILS} key={item.id} className="hover-scale ">
             <img
               src={item.background1x}
               srcSet={`${item.background1x} 1x, ${item.background2x} 2x`}
@@ -85,11 +170,11 @@ const DiscoveryMore = () => {
               <div className="flex justify-between items-center font-space-mono">
                 <div>
                   <p className="text-xs text-caption-label">Price</p>
-                  <p className="text-base">{item.price} ETH</p>
+                  <p className="text-base">{FormatBalance(item.price, 2)} ETH</p>
                 </div>
                 <div>
                   <p className="text-xs text-caption-label">Highest Bid</p>
-                  <p className="text-base">{item.bid} wETH</p>
+                  <p className="text-base">{FormatBalance(item.bid, 2)} wETH</p>
                 </div>
               </div>
             </div>
