fd2eab66611505ec6e3b39cfc2cd7b0189f193c4
VyDink <vy.dinh.2929@gmail.com>
2025-07-21 15:09:23 +0700

    Add responsive below 350px width

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -7,7 +7,7 @@ interface HeroCardProps {
 
 const HeroCard = ({ url1, url2, url3, className }: HeroCardProps) => {
   return (
-    <div className={`${className} w-[315px] h-[315px] `}>
+    <div className={`${className} w-[315px] h-[315px] max-[350px]:w-[275px] max-[350px]:h-[275px]`}>
       <img src={url1} srcSet={`${url1} 1x, ${url2} 2x`} alt="" />
       <div className="p-5 flex flex-col gap-2.5 bg-bg-secondary rounded-b-[20px]">
         <h5>Space Walking</h5>
