b14614b365a934b15be67c5894130e417d800d6c
VyDink <vy.dinh.2929@gmail.com>
2025-07-21 15:10:08 +0700

    Add responsive below 375px width

--- src/hooks/useResponsive.tsx
+++ src/hooks/useResponsive.tsx
@@ -2,12 +2,13 @@ import { useMediaQuery } from "react-responsive";
 
 export function useResponsive() {
   // Mobile: < 640px
-  const isMobile = useMediaQuery({ maxWidth: 639 });
+  const isMobile = useMediaQuery({ maxWidth: 639, minWidth: 375 });
+  const isMobileSmall = useMediaQuery({ maxWidth: 375 });
 
   // Tablet: >= 640px và < 1024px
   const isTablet = useMediaQuery({ minWidth: 640, maxWidth: 1023 });
 
   // Desktop: >= 1024px
   const isDesktop = useMediaQuery({ minWidth: 1024 });
-  return { isMobile, isDesktop, isTablet };
+  return { isMobileSmall, isMobile, isDesktop, isTablet };
 }
