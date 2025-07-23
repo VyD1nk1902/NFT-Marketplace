c55b22f1a3c552a07ad76f3babea241d1b86c46d
VyDink <vy.dinh.2929@gmail.com>
2025-07-23 17:22:11 +0700

    Refactor property to React.InputHTMLAttributes and return spread all props and ref={ref} for react-hook-form required

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,30 +1,31 @@
 import clsx from "clsx";
+import React from "react";
 
-interface InputProps {
-  id: string;
-  name: string;
-  className: string;
-  placeholder: string;
-  autoComplete: string;
-  type: "checkbox" | "email" | "number" | "text" | "password";
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
 }
+// React.InputHTMLAttributes<HTMLInputElement> đã bao gồm sẵn tất cả các props chuẩn của thẻ <input>
+// id: string;
+// name: string;
+// placeholder: string;
+// autoComplete: string;
+// type: "checkbox" | "email" | "number" | "text" | "password";
 
-const Inputs = ({ className, placeholder, type, id, name, autoComplete }: InputProps) => {
+const Inputs = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
   const baseClass = "flex items-center focus:outline-none";
 
   const finalInputClass = clsx(baseClass, className);
 
   return (
     <input
-      id={id}
-      name={name}
-      placeholder={placeholder}
-      type={type}
-      autoComplete={autoComplete}
+      ref={ref} // <-- BẮT BUỘC CHO REACT-HOOK-FORM
       className={finalInputClass}
+      {...props} //Spread all props, id, type, placeholder,...
     />
   );
-};
+});
+
+Inputs.displayName = "Inputs"; // Thêm dòng này để tránh warning với forwardRef
 
 export default Inputs;
 
