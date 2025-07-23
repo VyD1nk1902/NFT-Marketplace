9aac369e837c0f2b12574947567664e313f9e195
VyDink <vy.dinh.2929@gmail.com>
2025-07-23 17:25:40 +0700

    Add Scroll to top component

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import App from "./App.tsx";
 
 //BrowserRouter
 import { BrowserRouter } from "react-router-dom";
+// Auto Scroll to top
+import ScrollToTop from "@components/ScrollToTop.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
