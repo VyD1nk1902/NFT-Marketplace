b04d3054370476c71e8152560609baff6d971ced
VyDink <vy.dinh.2929@gmail.com>
2025-07-25 15:32:04 +0700

    Move clsx WrapClass from page to modules

--- src/modules/Marketplace/BrowseMaket.tsx
+++ src/modules/Marketplace/BrowseMaket.tsx
@@ -1,8 +1,31 @@
+import Inputs from "@components/Inputs";
+import { SearchIcon } from "@assets/Svg/SvgExport";
+
+import clsx from "clsx";
+import { useResponsive } from "@hooks/useResponsive";
+
 const BrowseMarket = () => {
+  const { isMobileSmall: MobileS, isMobile: Mobile, isTablet: Tablet, isDesktop: Desktop } = useResponsive();
+  const contentWrapClass = clsx({
+    "px-[115px] py-[80px]": Desktop,
+    "px-[77px] pt-[60px] pb-[80px]": Tablet,
+    "px-[30px] py-[40px]": Mobile || MobileS,
+  });
   return (
-    <div>
-      <div></div>
-    </div>
+    <section className="pt-[100px] w-full overflow-hidden content-wrapper">
+      <div className={`flex flex-col gap-[30px] ${contentWrapClass}`}>
+        <div>
+          <h2 className="lg:block hidden">Browse Marketplace</h2>
+          <h3 className="lg:hidden md:block hidden">Browse Marketplace</h3>
+          <h4 className="block md:hidden">Browse Marketplace</h4>
+          <p className="lg:text-[22px] text-base pt-2.5">Browse through more than 50k NFTs on the NFT Marketplace.</p>
+        </div>
+        <div className="flex py-3 px-5 items-center gap-2.5 rounded-[20px] border border-bg-secondary">
+          <Inputs placeholder="Search your favourite NFTs" className="w-full" />
+          <SearchIcon className="fill-white" />
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -1,9 +1,11 @@
 import BrowseMarket from "@modules/Marketplace/BrowseMaket";
+import NFTLists from "@modules/Marketplace/NFTLists";
 
 const Marketplace = () => {
   return (
     <div>
       <BrowseMarket />
+      <NFTLists />
     </div>
   );
 };
