cfe138bced24cd8d7fd412a90737e1ade833c1b2
VyDink <vy.dinh.2929@gmail.com>
2025-07-25 14:15:49 +0700

    Marketplace page init

---
+++ src/pages/Marketplace.tsx
@@ -0,0 +1,11 @@
+import BrowseMarket from "@modules/Marketplace/BrowseMaket";
+
+const Marketplace = () => {
+  return (
+    <div>
+      <BrowseMarket />
+    </div>
+  );
+};
+
+export default Marketplace;
