6303bb28b40b05034d31cdc8f37d5531195207c7
VyDink <vy.dinh.2929@gmail.com>
2025-07-25 18:12:09 +0700

    Export local img api for marketplace

---
+++ src/assets/Export/ImgExport.tsx
@@ -0,0 +1,109 @@
+// PNG
+import MagicMush1x from "@assets/Marketplace/MagicMush1x.png";
+import MagicMush2x from "@assets/Marketplace/MagicMush2x.png";
+import RobotBlue1x from "@assets/Marketplace/Robot(032)1x.png";
+import RobotBlue2x from "@assets/Marketplace/Robot(032)2x.png";
+import RobotYellow1x from "@assets/Marketplace/Robot(024)1x.png";
+import RobotYellow2x from "@assets/Marketplace/Robot(024)2x.png";
+import Bear1x from "@assets/Marketplace/Bear1x.png";
+import Bear2x from "@assets/Marketplace/Bear2x.png";
+import Dog1x from "@assets/Marketplace/Dog1x.png";
+import Dog2x from "@assets/Marketplace/Dog2x.png";
+import RobotDancing1x from "@assets/Marketplace/Robot(0312)1x.png";
+import RobotDancing2x from "@assets/Marketplace/Robot(0312)2x.png";
+import Blossom1x from "@assets/Marketplace/Blossom1x.png";
+import Blossom2x from "@assets/Marketplace/Blossom2x.png";
+import Travel1x from "@assets/Marketplace/Travel1x.png";
+import Travel2x from "@assets/Marketplace/Travel2x.png";
+import Sunset1x from "@assets/Marketplace/Sunset1x.png";
+import Sunset2x from "@assets/Marketplace/Sunset2x.png";
+import Desert1x from "@assets/Marketplace/Desert1x.png";
+import Desert2x from "@assets/Marketplace/Desert2x.png";
+import Monkey1x from "@assets/Marketplace/Monkey1x.png";
+import Monkey2x from "@assets/Marketplace/Monkey2x.png";
+
+// Avatar
+import Avatar from "@assets/Avatars/Avatar Placeholder.png";
+import Avatar1 from "@assets/Avatars/Avatar Placeholder (1).png";
+import Avatar2 from "@assets/Avatars/Avatar Placeholder (2).png";
+import Avatar3 from "@assets/Avatars/Avatar Placeholder (3).png";
+import Avatar4 from "@assets/Avatars/Avatar Placeholder (4).png";
+import Avatar5 from "@assets/Avatars/Avatar Placeholder (5).png";
+import Avatar6 from "@assets/Avatars/Avatar Placeholder (6).png";
+import Avatar7 from "@assets/Avatars/Avatar Placeholder (7).png";
+import Avatar8 from "@assets/Avatars/Avatar Placeholder (8).png";
+import Avatar9 from "@assets/Avatars/Avatar Placeholder (9).png";
+import Avatar10 from "@assets/Avatars/Avatar Placeholder (10).png";
+import Avatar11 from "@assets/Avatars/Avatar Placeholder (11).png";
+import Avatar12 from "@assets/Avatars/Avatar Placeholder (12).png";
+import Avatar13 from "@assets/Avatars/Avatar Placeholder (13).png";
+import Avatar14 from "@assets/Avatars/Avatar Placeholder (14).png";
+import Avatar15 from "@assets/Avatars/Avatar Placeholder (15).png";
+import Avatar16 from "@assets/Avatars/Avatar Placeholder (16).png";
+import Avatar17 from "@assets/Avatars/Avatar Placeholder (17).png";
+import Avatar18 from "@assets/Avatars/Avatar Placeholder (18).png";
+import Avatar19 from "@assets/Avatars/Avatar Placeholder (19).png";
+
+export const nftImagesMarketplace = {
+  MagicMushroom: {
+    background1x: MagicMush1x,
+    background2x: MagicMush2x,
+    artistIMG: Avatar6,
+  },
+  RobotBlue: {
+    background1x: RobotBlue1x,
+    background2x: RobotBlue2x,
+    artistIMG: Avatar11,
+  },
+  RobotYellow: {
+    background1x: RobotYellow1x,
+    background2x: RobotYellow2x,
+    artistIMG: Avatar11,
+  },
+  Bear: {
+    background1x: Bear1x,
+    background2x: Bear2x,
+    artistIMG: Avatar5,
+  },
+  Dog: {
+    background1x: Dog1x,
+    background2x: Dog2x,
+    artistIMG: Avatar14,
+  },
+  RobotDancing: {
+    background1x: RobotDancing1x,
+    background2x: RobotDancing2x,
+    artistIMG: Avatar7,
+  },
+  Blossom: {
+    background1x: Blossom1x,
+    background2x: Blossom2x,
+    artistIMG: Avatar15,
+  },
+  Travel: {
+    background1x: Travel1x,
+    background2x: Travel2x,
+    artistIMG: Avatar9,
+  },
+  Sunset: {
+    background1x: Sunset1x,
+    background2x: Sunset2x,
+    artistIMG: Avatar13,
+  },
+  Desert: {
+    background1x: Desert1x,
+    background2x: Desert2x,
+    artistIMG: Avatar19,
+  },
+  Monkey: {
+    background1x: Monkey1x,
+    background2x: Monkey2x,
+    artistIMG: Avatar17,
+  },
+  Dog2: {
+    background1x: Dog1x,
+    background2x: Dog2x,
+    artistIMG: Avatar12,
+  },
+  // ... thêm tất cả các ảnh khác
+};
