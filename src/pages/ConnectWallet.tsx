43af4436488cf8d7d761eb43c2680eab821792a4
VyDink <vy.dinh.2929@gmail.com>
2025-07-25 11:40:07 +0700

    Move ConnectWallet from modules to pages

---
+++ src/pages/ConnectWallet.tsx
@@ -0,0 +1,11 @@
+import ConnectWalletMain from "@modules/ConnectWallet/ConnectWalletMain";
+
+const ConnectWallet = () => {
+  return (
+    <div>
+      <ConnectWalletMain />
+    </div>
+  );
+};
+
+export default ConnectWallet;
