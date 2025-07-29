8684c0ef2c6e5e53b3c1608a577b0fcae22ce365
VyD <vy.dinh.2929@gmail.com>
2025-07-29 20:20:10 +0700

    Remove BrowseMarket module

--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -1,10 +1,8 @@
-import BrowseMarket from "@modules/Marketplace/BrowseMaket";
 import MarketplaceLists from "@modules/Marketplace/MarketplaceLists";
 
 const Marketplace = () => {
   return (
     <div>
-      <BrowseMarket />
       <MarketplaceLists />
     </div>
   );
