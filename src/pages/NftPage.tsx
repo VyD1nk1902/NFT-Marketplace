cc840d1f45b24ad6c0ec7f14c740bdb685ceeb3a
VyDink <vy.dinh.2929@gmail.com>
2025-08-04 16:52:01 +0700

    Rename module

--- src/pages/NftPage.tsx
+++ src/pages/NftPage.tsx
@@ -1,13 +1,13 @@
-import ArtistDetail from "@modules/NftPage/ArtistDetail";
-import ArtistMore from "@modules/NftPage/ArtistMore";
+import NFTDetail from "@modules/NftPage/NFTDetail";
+import NFTMore from "@modules/NftPage/NFTMore";
 import ImagePlaceholder from "@modules/NftPage/ImagePlaceholder";
 
 const NftPage = () => {
   return (
     <div>
       <ImagePlaceholder />
-      <ArtistDetail />
-      <ArtistMore />
+      <NFTDetail />
+      <NFTMore />
     </div>
   );
 };
