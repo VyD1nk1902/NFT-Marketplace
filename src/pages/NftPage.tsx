f46fbcb792ac48debdb3f7a2eb596b9c28454085
VyDink <vy.dinh.2929@gmail.com>
2025-08-04 16:49:04 +0700

    New nft page

---
+++ src/pages/NftPage.tsx
@@ -0,0 +1,15 @@
+import ArtistDetail from "@modules/NftPage/ArtistDetail";
+import ArtistMore from "@modules/NftPage/ArtistMore";
+import ImagePlaceholder from "@modules/NftPage/ImagePlaceholder";
+
+const NftPage = () => {
+  return (
+    <div>
+      <ImagePlaceholder />
+      <ArtistDetail />
+      <ArtistMore />
+    </div>
+  );
+};
+
+export default NftPage;
