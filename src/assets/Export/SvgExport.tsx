77e46097c1a5ebcdabc3a2aa2d79a17afccba80f
VyDink <vy.dinh.2929@gmail.com>
2025-08-04 18:31:58 +0700

    New icon for new Artist page

--- src/assets/Export/SvgExport.tsx
+++ src/assets/Export/SvgExport.tsx
@@ -339,3 +339,37 @@ export const ArrowRight = ({ className }: ExportSVGProps) => {
     </svg>
   );
 };
+
+export const Copy = ({ className }: ExportSVGProps) => {
+  return (
+    <svg width="32" height="32" viewBox="0 0 32 32" className={className} xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10 5C10 4.44772 10.4477 4 11 4H27C27.5523 4 28 4.44772 28 5V21C28 21.5523 27.5523 22 27 22H21C20.4477 22 20 21.5523 20 21C20 20.4477 20.4477 20 21 20H26V6H12V11C12 11.5523 11.5523 12 11 12C10.4477 12 10 11.5523 10 11V5Z"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 11C4 10.4477 4.44772 10 5 10H21C21.5523 10 22 10.4477 22 11V27C22 27.5523 21.5523 28 21 28H5C4.44772 28 4 27.5523 4 27V11ZM6 12V26H20V12H6Z"
+      />
+    </svg>
+  );
+};
+
+export const Plus = ({ className }: ExportSVGProps) => {
+  return (
+    <svg width="32" height="32" viewBox="0 0 32 32" className={className} xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 16C4 15.4477 4.44772 15 5 15H27C27.5523 15 28 15.4477 28 16C28 16.5523 27.5523 17 27 17H5C4.44772 17 4 16.5523 4 16Z"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16 4C16.5523 4 17 4.44772 17 5V27C17 27.5523 16.5523 28 16 28C15.4477 28 15 27.5523 15 27V5C15 4.44772 15.4477 4 16 4Z"
+      />
+    </svg>
+  );
+};
