70745a36f652a43d9cbbb8da07a2c5ae88ccd23a
VyDink <vy.dinh.2929@gmail.com>
2025-08-04 18:32:37 +0700

    New page

---
+++ src/pages/ArtistPage.tsx
@@ -0,0 +1,13 @@
+import ArtistInfo from "@modules/ArtistPage/ArtistInfo";
+import CoverImg from "@modules/ArtistPage/CoverImg";
+
+const ArtistPage = () => {
+  return (
+    <div>
+      <CoverImg />
+      <ArtistInfo />
+    </div>
+  );
+};
+
+export default ArtistPage;
